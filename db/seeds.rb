1e7441c2b83a8cb54fe704023750cd3c243409b6
Fanny Maurel <fannymaurel@outlook.com>
2019-02-15 12:16:22 +0100

    config database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(
+  name: "White Russian",
+  cocktail_image_url: "https://images.pexels.com/photos/45237/cocktail-glass-glass-strawberry-cream-45237.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500")
