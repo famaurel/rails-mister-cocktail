545c6b87ad6cd6c443009aab0c37c34884cbccad
Fanny Maurel <fannymaurel@outlook.com>
2019-02-16 22:18:16 +0100

    cloudinary fix

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dwsnzdr34'
+  config.api_key = '244874595543165'
+  config.api_secret = 'd3YzcFeKBLW4wmbx6hdjdCjWvc0'
+  config.secure = true
+  config.cdn_subdomain = true
+end
